d4c2ac46c8f21f6675cd0793d0a0ee3c7493c920
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-12-14 17:39:10 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils import *
+import os
 
 
-def union(left, right):
+def intersection(left, right):
     u = []
     for el in left:
         for er in right:
@@ -15,21 +16,80 @@ def left_not_right(left, right):
     for el in left:
         match = False
         for er in right:
-            if el[0][0] == er[0][0]:
+            if el[0] == er[0]:
                 match = True
         if not match:
             l.append(el)
     return l
 
+
+
+def right_not_left(left, right):
+    l = []
+    for el in right:
+        match = False
+        for er in left:
+            if el[0] == er[0]:
+                match = True
+        if not match:
+            l.append(el)
+    return l
+
+
+
+def flush_file(path):
+    path = os.getcwd()
+    fl = ['union.csv', 'left-not-right.csv', 'right-not-left.csv', 'or.csv']
+    for file in fl:
+        try:
+            os.remove(path + '/files/' + file)
+        except:
+            pass
+
+def union(left, right):
+    l=[]
+
+    for el in left:
+        for er in right:
+            if el[0]==er[0]:
+                if not contains_element(l, el[0]):
+                    l.append(el)
+            else:
+                if not contains_element(l, el[0]):
+                    l.append(el)
+                if not contains_element(l, er[0]):
+                    l.append(er)
+    # now do a distinct
+    # I still have to figure out how this whole set thing atually works
+    # l=list(set(l))  - this did not work
+    return l
+
+
+
+def contains_element (l, e):
+    for row in l:
+        if row[0]==e:
+            return True
+    return False
+
+
+
+
 def main():
-    left = csv2list('Left.csv')
-    right = csv2list('Right.csv')
-    print(left)
-    print(right)
-    # u = union(left, right)
+    path = os.getcwd()
+    flush_file(path)
+    left = csv2list(path + '/files/Left.csv')
+    right = csv2list(path + '/files/Right.csv')
+    l = intersection(left, right)
+    list2csv(l, path + "/files/union.csv")
     # print("union", u)
     l = left_not_right(left, right)
-    print (l)
+    list2csv(l, path + "/files/left-not-right.csv")
+    l=right_not_left (left, right)
+    list2csv(l,path + '/files/right-not-left.csv')
+    l=union(left,right )
+    list2csv(l, path + '/files/union.csv')
+
 
 
 main()
--- utils.py
+++ utils.py
@@ -10,3 +10,34 @@ def csv2list(fn):
             line.append(token)
         l.append(line)
     return l
+
+
+
+def list2csv(l, fn):
+    f=open(fn, 'w')
+    write=csv.writer(f)
+    for ll in l: # ll  list element, maybe I shoulda done le, not sure
+        write.writerow(ll)
+    
+
+'''
+
+
+# field names
+fields = ['Name', 'Branch', 'Year', 'CGPA']
+
+# data rows of csv file
+rows = [['Nikhil', 'COE', '2', '9.0'],
+        ['Sanchit', 'COE', '2', '9.1'],
+        ['Aditya', 'IT', '2', '9.3'],
+        ['Sagar', 'SE', '1', '9.5'],
+        ['Prateek', 'MCE', '3', '7.8'],
+        ['Sahil', 'EP', '2', '9.1']]
+
+with open('GFG.csv', 'w') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+    write.writerow(fields)
+    write.writerows(rows)
+
+'''
